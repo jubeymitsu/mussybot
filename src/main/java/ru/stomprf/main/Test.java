b6bb81cbb7faa2e7e93184f409bb321d7284e911
jubeymitsu <jubey.mitsu@gmail.com>
2023-03-28 18:38:21 +0300

    Parser added

--- src/main/java/ru/stomprf/main/Test.java
+++ src/main/java/ru/stomprf/main/Test.java
@@ -1,21 +1,14 @@
 package ru.stomprf.main;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.HttpResponse;
-import org.apache.hc.core5.http.protocol.BasicHttpContext;
-
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
 
 public class Test {
 
     static String FILE_URL = "https://musify.club/track/dl/12711722/doja-cat-bottom-bitch.mp3";
-    static String FILE_NAME = "path-to-source/doja_cat-track.mp3";
+    static String FILE_NAME = "/home/james/Desktop/projects/mussybot/src/main/resources/doja_cat-track.mp3";
 
     public static void main(String[] args) throws IOException {
 
@@ -29,16 +22,26 @@ public class Test {
 ////Save file on disk
 ////        copyInputStreamToFile(response.getEntity().getContent(), fileToSave);
 
-        try (BufferedInputStream in = new BufferedInputStream(new URL(FILE_URL).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
-            byte dataBuffer[] = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-            }
-        } catch (IOException e) {
-            // handle exception
+        try (final WebClient webClient = new WebClient()) {
+            final HtmlPage page = webClient.getPage("https://htmlunit.sourceforge.io/");
+            System.out.println(page.getTitleText());
+
         }
+        catch (Exception ec){
+
+        }
+
+//      D O W N L O A D
+//        try (BufferedInputStream in = new BufferedInputStream(new URL(FILE_URL).openStream());
+//             FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
+//            byte dataBuffer[] = new byte[1024];
+//            int bytesRead;
+//            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+//                fileOutputStream.write(dataBuffer, 0, bytesRead);
+//            }
+//        } catch (IOException e) {
+//            // handle exception
+//        }
 
     }
 
